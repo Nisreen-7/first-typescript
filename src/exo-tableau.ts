1f90c1567a8f81e7f941ed24577a694b81772e79
nisreen <nisokh7@gmail.com>
2023-05-23 13:45:33 +0200

    commit de test

--- src/exo-tableau.ts
+++ src/exo-tableau.ts
@@ -1,19 +1,23 @@
-// let Ages=[20,18,40,50];
+// let Ages:numbers[]=[20,18,40,50];
 // for (const item of Ages) {
-//     if(item > 18)
+//     if(item >= 18)
 //     console.log("WELCOME");
-//     else
-//     console.log("you is too young");
+//     else{
+//     console.log(item + "you is too young");
 
 // }
 
-let Ages = [20, 18, 40, 50];
+// }
+
+let Ages = [20, 18, 30, 40];
 for (let i = 0; i < Ages.length; i++) {
-    if (Ages[i] > 18) {
+    const item = Ages[i];
+    if (item > 18) {
         console.log("WELCOME");
         const newDiv = document.createElement("div");
         const newContent = document.createTextNode("WELCOME");
         newDiv.appendChild(newContent);
+        document.body.append(newDiv);
     }
 
     else {
@@ -21,6 +25,8 @@ for (let i = 0; i < Ages.length; i++) {
         const newSpan = document.createElement("span");
         const newContent = document.createTextNode("you is too young");
         newSpan.appendChild(newContent);
+        document.body.append(newSpan);
+
 
     }
 }
