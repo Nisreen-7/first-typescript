ae52d7cf5fd488db9d22ccbe0ea572af8af852de
nisreen <nisokh7@gmail.com>
2023-05-24 09:59:23 +0200

    commit final object

--- src/exo-objet.ts
+++ src/exo-objet.ts
@@ -3,17 +3,21 @@ interface Dog {
     breed: string,
     hunger: number,
 }
-
+/** 
+ * 
+ * @param a le chien a nourrir  
+ */
 function feedDog(a: Dog) {
 
-    if (a.hunger <= 0) {
+    if (a.hunger <= 25) {
 
         console.log(a.name + " dog is not hungry" + "  " + a.hunger);
-
+        document.write(`<h3> ${a.name} </h3>`);
     }
     else {
-        a.hunger += -50;
+        a.hunger -= 50;
         console.log(a.name + " is eating, good dog" + "  " + a.hunger);
+        document.write(`<h3> ${a.name} </h3>`);
 
     }
 
@@ -31,4 +35,9 @@ feedDog(chien);
 feedDog(chien);
 feedDog(chien);
 feedDog(chien);
-feedDog(chien); 
+feedDog(chien);
+feedDog({
+    name: "rex",
+    breed: "dalmation",
+    hunger: 0,
+})
